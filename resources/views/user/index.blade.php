afe069523f787a361ad4122480eca86641ce154f
HafidzDwiRahmansyah <dwihafidz009@gmail.com>
2024-07-28 07:23:42 +0700

    website_pentatech.id

---
+++ resources/views/user/index.blade.php
@@ -0,0 +1,13 @@
+@extends('template.interface2')
+
+@section('title','Landing Page')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-lg-6">
+
+        </div>
+    </div>
+</div>
+@endsection
